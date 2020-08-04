1eecfcad0bd564e8d1490f21d12e75a19b0842a7
Bomza91 <bmasentile@gmail.com>
2020-08-04 15:39:19 +0200

    initial changes

---
+++ greet.js
@@ -0,0 +1,15 @@
+
+// function greet(name){
+   
+   // var msg = greet('Xola')
+//  return "Hello, "+ name;
+
+ module.exports = function(name){
+    return 'Hello, ' + name;
+}
+    
+    
+    // }
+    // console.log(greet('Xola'))
+
+    
